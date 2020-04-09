e05c99a0b291e4b80434fcf2ddac4bd91fe45342
Rwanda S <rwanda.ace@gmail.com>
2020-04-09 16:20:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
 class Song
-  extend Memorable
+  extend Memorable::ClassMethods
   extend Findable
   include Paramable
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
